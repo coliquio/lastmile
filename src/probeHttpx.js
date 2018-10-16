4f6c68231b577abb38fc7b9fb83be10fba1485ad
Jens Bissinger <mail@jens-bissinger.de>
2018-10-16 15:14:33 +0200

    DRY probeHttpx

---
+++ src/probeHttpx.js
@@ -0,0 +1,58 @@
+const measureDurationInMs = require('./measureDurationInMs');
+const probeStatus = require('./probeStatus');
+const matchHttpExpectation = require('./matchHttpExpectation');
+module.exports = httpOrHttps => async (config) => {
+  const getDurationInMs = measureDurationInMs();
+  const options = {
+    host: config.host,
+    port: config.port,
+    path: config.path,
+    method: config.method || 'GET',
+    headers: {}
+  };
+  if (config.userAgent) options.headers['user-agent'] = config.userAgent;
+  if (typeof config.tls === 'object' && typeof config.tls.ca === 'string') {
+    options.agent = new httpOrHttps.Agent({
+      ca: config.tls.ca
+    });
+  }
+  return new Promise((resolvePromise) => {
+    let result = {};
+    let timedOut = false;
+    const resolve = (value) => {
+      clearTimeout(timeout);
+      resolvePromise(value);
+    };
+    const request = httpOrHttps.request(options, (res) => {
+      const expectationMatch = matchHttpExpectation(config, res);
+      result.probe_status = expectationMatch.ok ? probeStatus.ok : probeStatus.failedExpectation;
+      if (!expectationMatch.ok) result.probe_failed_expectations = expectationMatch.failedExpectations.join(',');
+      result.res_status = res.statusCode;
+      result.socket_dst_family = res.socket.remoteFamily;
+      result.socket_dst_address = res.socket.remoteAddress;
+      result.socket_src_family = res.socket.address().family;
+      result.socket_src_address = res.socket.address().address;
+      if (typeof res.socket.getProtocol === 'function') {
+        result.socket_tls_procotol = res.socket.getProtocol();
+      }
+      res.on('data', () => {});
+      res.on('end', () => {
+        resolve(Object.assign({
+          duration: getDurationInMs()
+        }, result));
+      });
+    });
+    request.on('error', (e) => {
+      resolve(Object.assign({
+        probe_status: probeStatus.error,
+        err_code: timedOut ? 'TIMEOUT' : e.code,
+        duration: getDurationInMs()
+      }, result));
+    });
+    request.end();
+    const timeout = setTimeout(() => {
+      timedOut = true;
+      request.abort();
+    }, config.timeout || 10000);
+  });
+};
