9cd7606f1d9a9104d5da9e0eb5b4bb14d4969b0d
Jens Bissinger <mail@jens-bissinger.de>
2021-10-27 12:52:04 +0200

    Use async / promise

--- src/loadProbesConfig.js
+++ src/loadProbesConfig.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const axios = require('axios');
 const parseProbeConfig = require('./parseProbeConfig');
 
-const loadConfigFromFile = (path) => {
-  return JSON.parse(fs.readFileSync(path, 'utf8'));
+const loadConfigFromFile = async (path) => {
+  return JSON.parse(await fs.readFile(path, 'utf8'));
 };
 
 const loadConfigFromHttp = (url) => {
