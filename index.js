d1212572937b6a71dbe5642d7ab5bbed5460513f
Jens Bissinger <mail@jens-bissinger.de>
2021-10-27 12:12:33 +0200

    Refactor index.js run

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+const config = require('./src/config')();
+const run = require('./src/run');
+const evaluateMetrics = require('./src/evaluateMetrics');
+
 process.on('exit', function() {
   console.log('Stopping worker');
 });
+
 process.on('SIGINT', function() {
   console.log('SIGINT, Stopping worker');
   process.exit(1);
 });
 
-const config = require('./src/config')();
-const run = require('./src/run');
-const evaluateMetrics = require('./src/evaluateMetrics');
-
-const firstRun = run(config);
-if (config.probeOneShot) {
-  firstRun.then(metrics => {
+async function main() {
+  const firstRun = run(config);
+  if (config.probeOneShot) {
+    const metrics = await firstRun
     const {status, exitCode} = evaluateMetrics(metrics);
     console.log(`Probes finished with status = ${status} (${exitCode})`);
     process.exit(exitCode);
-  });
-} else {
-  setInterval(() => {
-    run(config);
-  }, config.probeInterval);
+  } else {
+    setInterval(() => {
+      run(config);
+    }, config.probeInterval);
+  }
 }
+main()
