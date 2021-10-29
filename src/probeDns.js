6ef820e673d79aef6608c62e16bf0013eec52c0f
Jens Bissinger <mail@jens-bissinger.de>
2021-10-29 14:39:49 +0200

    Refactor probeDns

--- src/probeDns.js
+++ src/probeDns.js
@@ -1,6 +1,44 @@
 const measureDurationInMs = require('./measureDurationInMs');
 const probeStatus = require('./probeStatus');
 const { Resolver } = require('dns');
+
+const PROBE_RESULT_TIMEOUT = {
+  err_code: 'TIMEOUT',
+  probe_status: probeStatus.error
+};
+
+function buildProbeResultForError(expect, err) {
+  const result = {};
+  result.err_code = err.code;
+  if (expect.err_code) {
+    if (expect.err_code == err.code) {
+      result.probe_status = probeStatus.ok;
+    } else {
+      result.probe_status = probeStatus.failedExpectation;
+      result.probe_failed_expectations = 'ERR_CODE';
+    }
+  } else {
+    result.probe_status = probeStatus.error;
+  }
+  return result;
+}
+
+function buildProbeResultForResolvedAddresses(expect, rawAddresses) {
+  const result = {};
+  const addresses = rawAddresses.map(a => a.value || a.address);
+  result.res_addresses = addresses.join(',');
+  if (expect.address && addresses.indexOf(expect.address) < 0) {
+    result.probe_status = probeStatus.failedExpectation;
+    result.probe_failed_expectations = 'ADDRESS';
+  } else if (expect.err_code) {
+    result.probe_status = probeStatus.failedExpectation;
+    result.probe_failed_expectations = 'ERR_CODE';
+  } else {
+    result.probe_status = probeStatus.ok;
+  }
+  return result;
+}
+
 module.exports = (config, DNSResolver = Resolver) => {
   return new Promise((resolvePromise) => {
     let timedOut = false;
@@ -11,38 +49,19 @@ module.exports = (config, DNSResolver = Resolver) => {
     const getDurationInMs = measureDurationInMs();
     const resolver = new DNSResolver();
     resolver.resolveAny(config.host, (err, rawAddresses) => {
-      const result = {
-        duration: getDurationInMs()
-      };
+      const duration = getDurationInMs();
+      let result;
       if (timedOut) {
-        result.err_code = 'TIMEOUT';
-        result.probe_status = probeStatus.error;
+        result = PROBE_RESULT_TIMEOUT;
       } else if (err) {
-        result.err_code = err.code;
-        if (config.expect.err_code) {
-          if (config.expect.err_code == err.code) {
-            result.probe_status = probeStatus.ok;
-          } else {
-            result.probe_status = probeStatus.failedExpectation;
-            result.probe_failed_expectations = 'ERR_CODE';
-          }
-        } else {
-          result.probe_status = probeStatus.error;
-        }
+        result = buildProbeResultForError(config.expect, err);
       } else {
-        const addresses = rawAddresses.map(a => a.value || a.address);
-        result.res_addresses = addresses.join(',');
-        if (config.expect.address && addresses.indexOf(config.expect.address) < 0) {
-          result.probe_status = probeStatus.failedExpectation;
-          result.probe_failed_expectations = 'ADDRESS';
-        } else if (config.expect.err_code) {
-          result.probe_status = probeStatus.failedExpectation;
-          result.probe_failed_expectations = 'ERR_CODE';
-        } else {
-          result.probe_status = probeStatus.ok;
-        }
+        result = buildProbeResultForResolvedAddresses(config.expect, rawAddresses);
       }
-      resolve(result);
+      resolve({
+        duration,
+        ...result
+      });
     });
     const timeout = setTimeout(() => {
       timedOut = true;
