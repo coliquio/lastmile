36ccc0c5c0524544a2bb5df8ed15005ab1d17399
Jens Bissinger <mail@jens-bissinger.de>
2022-11-25 16:31:55 +0100

    Fix docker

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker build --build-arg GIT_REF=$(git rev-parse --short HEAD) -t coliquiode/lastmile .
+VERSION=1.1.0
+docker build --build-arg GIT_REF=$(git rev-parse --short HEAD) -t coliquiode/lastmile:$VERSION .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push coliquiode/lastmile:1.1.0
+docker push coliquiode/lastmile:$VERSION
