2a468a9418f0d0579ca28b921daf1d7a856cf9d6
Jens Bissinger <mail@jens-bissinger.de>
2022-12-23 10:33:15 +0100

    Push full version, stable version and latest

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-VERSION=1.2.1
-docker build --build-arg GIT_REF=$(git rev-parse --short HEAD) -t coliquiode/lastmile:$VERSION .
+MAJOR=1.2
+MINOR=1
+STABLE_VERSION="${MAJOR}-stable"
+FULL_VERSION="${MAJOR}.${MINOR}"
+
+docker build --build-arg GIT_REF=$(git rev-parse --short HEAD) -t coliquiode/lastmile:$FULL_VERSION .
+docker tag coliquiode/lastmile:$FULL_VERSION coliquiode/lastmile:$STABLE_VERSION
+docker tag coliquiode/lastmile:$FULL_VERSION coliquiode/lastmile:latest
+
+echo "Building latest $STABLE_VERSION $FULL_VERSION"
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag coliquiode/lastmile:$VERSION coliquiode/lastmile:latest
-docker push coliquiode/lastmile # all tags
+docker push coliquiode/lastmile --all-tags
